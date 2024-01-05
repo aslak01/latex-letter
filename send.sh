88b8dd6832d88c969d741ab7e53c5d96f22f017c
as <aslak.bakkeland@medialaben.no>
2024-01-05 01:08:49 +0100

    diff

--- send.sh
+++ send.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <parameter>"
-	exit 1
+    echo "Usage: $0 <parameter>"
+    exit 1
 fi
 
 if [ ! -f "output.pdf" ]; then
-	echo "Error: The file 'output.pdf' does not exist."
-	exit 1
+    echo "Error: The file 'output.pdf' does not exist."
+    exit 1
 fi
 
 # ensure there is a parent dir
@@ -19,11 +19,11 @@ filename="$recipient.pdf"
 destination="$parentDir/$recipient"
 
 if [ -d "$destination" ]; then
-	counter=1
-	while [ -d "$destination-$counter" ]; do
-		counter=$((counter + 1))
-	done
-	destination="$destination-$counter"
+    counter=1
+    while [ -d "$destination-$counter" ]; do
+        counter=$((counter + 1))
+    done
+    destination="$destination-$counter"
 fi
 
 mv output.pdf "$filename"
